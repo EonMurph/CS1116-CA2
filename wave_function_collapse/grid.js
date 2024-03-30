be9fd36a7b9ab86c29808bb9810baf8e5ff4eda4
EonMurph <stiger.home@gmail.com>
2024-03-30 12:37:00 +0000

    initialised tiles and grid variables

---
+++ wave_function_collapse/grid.js
@@ -0,0 +1,17 @@
+import { BLANK, UP, RIGHT, DOWN, LEFT, tileSize } from "./tiles.js";
+export const grid = [];
+export const DIM = 50;
+for (let r = 0; r < DIM; r++) {
+  for (let c = 0; c < DIM; c++) {
+    let cell = {
+      tile: null,
+      options: [BLANK, UP, RIGHT, DOWN, LEFT],
+      x: tileSize * c,
+      y: tileSize * r,
+      r,
+      c,
+    };  
+    cell.entropy = cell.options.length;
+    grid.push(cell);
+  }
+}
---
+++ wave_function_collapse/tiles.js
@@ -0,0 +1,59 @@
+export const tiles = [];
+export const BLANK = 0;
+export const UP = 1;
+export const RIGHT = 2;
+export const DOWN = 3;
+export const LEFT = 4;
+
+export const tileSize = 64;
+
+tiles[BLANK] = {
+  tile: "blank",
+  image: new Image(),
+  rules: [
+    [BLANK, UP], // these tiles can be placed above
+    [BLANK, RIGHT], // these tiles can be placed to the right
+    [BLANK, LEFT], // these tiles can be placed to the left
+    [BLANK, DOWN], // these tiles can be placed under
+  ],
+};
+tiles[UP] = {
+  tile: "up",
+  image: new Image(),
+  rules: [
+    [RIGHT, DOWN, LEFT], // up
+    [UP, DOWN, LEFT], // right
+    [UP, RIGHT, DOWN], // left
+    [BLANK, DOWN], // down
+  ],
+};
+tiles[RIGHT] = {
+  tile: "right",
+  image: new Image(),
+  rules: [
+    [RIGHT, DOWN, LEFT], // up
+    [UP, DOWN, LEFT], // right
+    [BLANK, LEFT], // left
+    [UP, RIGHT, LEFT], // left
+  ],
+};
+tiles[DOWN] = {
+  tile: "down",
+  image: new Image(),
+  rules: [
+    [BLANK, UP], // up
+    [UP, DOWN, LEFT], // right
+    [UP, RIGHT, DOWN], // left
+    [UP, RIGHT, LEFT], // down
+  ],
+};
+tiles[LEFT] = {
+  tile: "left",
+  image: new Image(),
+  rules: [
+    [RIGHT, DOWN, LEFT], // up
+    [BLANK, RIGHT], // right
+    [UP, RIGHT, DOWN], // left
+    [UP, RIGHT, LEFT], // down
+  ],
+};
